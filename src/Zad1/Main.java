94d127cb7a0ce4c0a21d5181e89dcd2e021e565a
mihal <michal18q@gmail.com>
2020-09-17 10:24:31 +0200

    Zad1 refactored

--- src/Zad1/Main.java
+++ src/Zad1/Main.java
@@ -4,33 +4,26 @@ import java.io.*;
 
 public class Main {
 
-    private static String fileName = "input.txt";
+    private static final String fileName = "src/Zad1/input.txt";
 
     public static void main(String[] args) {
-        System.out.println("Start");
+
         int totalFuel = 0;
+
         try {
             BufferedReader reader = new BufferedReader(new FileReader(fileName));
-            String line = reader.readLine();
-            while(line != null) {
+            String line;
+            while((line = reader.readLine()) != null) {
                 int mass = Integer.parseInt(line);
-                System.out.print(mass);
-
                 int fuelAmount = calculateRequiredFuel(mass);
                 fuelAmount += calculateFuelRequiredToHandelPreviousFuel(fuelAmount);
                 totalFuel += fuelAmount;
-                System.out.println("  total: " + totalFuel);
-                line = reader.readLine();
             }
-
-
         } catch (IOException e) {
             e.printStackTrace();
         }
 
         System.out.println("Fuel needed for equipment: " + totalFuel);
-        //System.out.println("Total fuel needed:" + (totalFuel + calculateFuelRequiredToHandelPreviousFuel(totalFuel)) );
-        //4846752
     }
 
     private static int calculateRequiredFuel(int mass) {
@@ -38,15 +31,9 @@ public class Main {
     }
 
     private static int calculateFuelRequiredToHandelPreviousFuel(int fuel) {
-        System.out.print("For " + fuel + " we need extra ");
         fuel = calculateRequiredFuel(fuel);
-        System.out.println(fuel + " of fuel");
-
-        if (fuel > 5) {
+        if (fuel > 5)
             fuel += calculateFuelRequiredToHandelPreviousFuel(fuel);
-        }
-        System.out.println("Zwracany jest: " + fuel);
-
         return fuel;
     }
 }
